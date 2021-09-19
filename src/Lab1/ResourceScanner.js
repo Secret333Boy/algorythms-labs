2fa1b77d2305b72ad981930e8a88cec91295a995
Secret333Boy <pashkovskiy.eugen@gmail.com>
2021-09-20 02:20:00 +0300

    if statements swap

--- src/Lab1/ResourceScanner.js
+++ src/Lab1/ResourceScanner.js
@@ -1,13 +1,13 @@
 'use strict';
 
 process.on('message', msg => {
-  if (msg === 'end') {
-    console.timeEnd('Solution');
-    process.exit();
-  } else if (msg === 'start') {
+  if (msg === 'start') {
     console.time('Solution');
     setInterval(() => {
       console.timeLog('Solution');
     }, 10);
+  } else if (msg === 'end') {
+    console.timeEnd('Solution');
+    process.exit();
   }
 });
