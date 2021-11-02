108d974b7bffcdcdf5d05bd3db67f7a7e2ac0f61
Secret333Boy <pashkovskiy.eugen@gmail.com>
2021-11-02 19:51:59 +0200

    Lab3 directory init

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@
 const labs = [];
 labs.push(require('./Lab1/Lab1.js'));
 labs.push(require('./Lab2/Lab2.js'));
+labs.push(require('./Lab3/Lab3.js'));
 const stdinput = require('./stdinput.js');
 
-console.log('What lab do you want to execute? [1, 2]');
+console.log('What lab do you want to execute? [1, 2, 3]');
 
 stdinput().then(line => {
   const num = +line;
